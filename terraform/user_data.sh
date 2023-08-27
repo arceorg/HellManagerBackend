626f39b1a97fb9433d93d1b1b03b8cc8fd575a74
Juan David Arce Martinez <juan.arce@instaleap.io>
2023-08-27 15:57:54 -0500

    [REFACTOR] terraform

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd home/ec2-user/
 mkdir app
-sudo chown -R ec2-user app
-cd app
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker
 sudo yum install git -y
 
 git clone --branch main https://github.com/arceorg/HellManagerBackend.git
-cd HellManagerBackend
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.13.0
+sudo chown -R ec2-user app
+cd app/HellManagerBackend
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
 npm i
 npm run build-prod
 npm run start-prod
