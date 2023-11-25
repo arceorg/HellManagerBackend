b25f84a0427546a98f5d7051b7e276fa417fb783
JuanDavidArc <117832490+JuanDavidArc@users.noreply.github.com>
2023-11-25 10:17:53 -0500

    [FIX] deploy (#11)

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -15,7 +15,6 @@ sudo chown -R ec2-user ../app
 cd HellManagerBackend
 sudo su
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
 nvm install
 npm i
 npm run build-prod
